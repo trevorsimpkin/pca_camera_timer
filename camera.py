c9454ff6600ca2f87910dff4fa94f6831de9f086
Trevor Simpkin <TrevorSimpkin@gmail.com>
2018-10-11 17:47:17 -0400

    changes to how input is received; added camera;

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
 from time import sleep
 from datetime import datetime, timedelta
-# from picamera import PiCamera
+from picamera import PiCamera
 
 
 class Camera:
@@ -8,19 +8,31 @@ class Camera:
         self.start_time = start_time
         self.duration_hour = duration_hour
         self.duration_min = duration_min
+        self.camera = picamera.PiCamera()
+        self.fileName = self.start_time.strftime("%Y-%m-%d %H:%M")
+
+
+
 
     def get_start_time(self):
         print(self.start_time)
 
     def start_timer(self):
+        print(self.start_time.strftime("%Y-%m-%d %H:%M:%S"))
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        camera.resolution = (1296, 730)
+        print("%d" % self.duration_min)
         while self.start_time.strftime("%Y-%m-%d %H:%M:%S") != datetime.now().strftime("%Y-%m-%d %H:%M:%S"):
             sleep(1)
 
         print("They are equal")
+
         duration = self.duration_hour*3600
         duration += self.duration_min*60
-        print("%d", duration)
-        sleep(duration)
+        print("%d" % duration)
+        camera.start_recording(filename + '_test.h264')
+        camera.wait_recording(duration)
+        camera.stop_recording()
         print("time has gone by")
 
 
--- pca_camera_timer.py
+++ pca_camera_timer.py
@@ -1,43 +1,20 @@
 from datetime import datetime
 from camera import Camera
-from guizero import App, Text, Picture, Box, TextBox, Combo, PushButton
+import sys
 # Will need to do switch case on month to determin how many days of each month when entered.
 #  Maybe don't show day until month chosen if possible
 
 
-def schedule_camera():
-    start_time_input = datetime(year=int(schedule_start_time_year.get()), month=int(schedule_start_time_month.get()),
-                          day=int(schedule_start_time_day.get()), hour=int(schedule_start_time_hour.get()),
-                          minute=int(schedule_start_time_minute.get()), second=0)
-    duration_hour_input = int(duration_hours.get())
-    duration_min_input = int(duration_minutes.get())
-    pi_camera = Camera(start_time=start_time_input, duration_hour=duration_hour_input, duration_min=duration_min_input)
-    pi_camera.get_start_time()
+def schedule_camera(start_time_init, duration_hour_init, duration_min_init):
+    pi_camera = Camera(start_time=start_time_init, duration_hour=duration_hour_init, duration_min=duration_min_init)
     pi_camera.start_timer()
 
 
-app = App(title="PCA Traffic Counting Camera")
-logo = Picture(app, image="pca.png")
-schedule_camera_text = Text(app, text="Schedule Camera", font="Helvetica", size=20)
-container = Box(app, layout="grid")
-start_time_text = Text(container, text="Enter Start Date: ", font="Helvetica", size=20, grid=[0, 0])
-start_time_text_month = Text(container, text="Month: ", font="Helvetica", size=20, grid=[1, 0])
-schedule_start_time_month = Combo(container, options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], grid=[2, 0])
-start_time_text_day = Text(container, text=" Day: ", font="Helvetica", size=20, grid=[3, 0])
-schedule_start_time_day = Combo(container, options=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                                                    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], grid=[4, 0])
-start_time_text_year = Text(container, text="Year: ", font="Helvetica", size=20, grid=[5, 0])
-schedule_start_time_year = Combo(container, options=[2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025], grid=[6, 0])
-start_time_text_hour = Text(container, text=" Hour: ", font="Helvetica", size=20, grid=[7, 0])
-schedule_start_time_hour = Combo(container, options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-                                                     19, 20, 21, 22, 23], grid=[8, 0])
-schedule_start_time_minute = TextBox(container, grid=[9, 0])
-duration_text = Text(container, text="Enter Duration Hours: ", font="Helvetica", size=20, grid=[0, 1])
-duration_hours = Combo(container, options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], grid=[1, 1])
-duration_minutes = Combo(container, options=[0, 15, 30, 45], grid=[1, 2])
-# app.tk.attributes("-fullscreen", True)
-button = PushButton(app, command=schedule_camera)
-
-app.display()
+start_time = datetime(year=int(sys.argv[1]), month=int(sys.argv[2]),
+                      day=int(sys.argv[3]), hour=int(sys.argv[4]),
+                      minute=int(sys.argv[5]), second=0)
+duration_hour = int(sys.argv[6])
+duration_min = int(sys.argv[7])
+schedule_camera(start_time, duration_hour, duration_min)
 
 
