1ece3e30df81d824d653898fa4475e27ab0e07b2
Trevor Simpkin <TrevorSimpkin@gmail.com>
2018-10-13 15:46:47 -0400

    Update camera object;

--- camera.py
+++ camera.py
@@ -6,11 +6,13 @@ from picamera import PiCamera
 class Camera:
     def __init__(self, start_time, duration_hour, duration_min):
         self.camera = picamera.PiCamera()
-        camera.resolution = (1296, 730)
+        self.camera.resolution = (1296, 730)
         self.start_time = start_time
         self.duration_hour = duration_hour
         self.duration_min = duration_min
         self.fileName = self.start_time.strftime("%Y-%m-%d_%H:%M.h264")
+        self.duration = self.duration_hour * 3600
+        self.duration += self.duration_min * 60
 
 
 
@@ -26,11 +28,10 @@ class Camera:
             sleep(1)
         print("They are equal")
 
-        duration = self.duration_hour*3600
-        duration += self.duration_min*60
-        print("%d" % duration)
-        camera.start_recording(filename)
-        camera.wait_recording(duration)
+
+        print("%d" % self.duration)
+        self.camera.start_recording(self.filename)
+        self.camera.wait_recording(self.duration)
         camera.stop_recording()
         print("time has gone by")
 
